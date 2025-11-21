c1620bf2af120553e43a36f3dfd0a4dfe92c53d7
Leonid Gordenin <db0zmail@gmail.com>
2025-11-21 17:56:24 -0300

    paddle tilting, stretching, music

--- Assets/Scripts/PlayerPaddle.cs
+++ Assets/Scripts/PlayerPaddle.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerPaddle : MonoBehaviour
@@ -6,22 +8,73 @@ public class PlayerPaddle : MonoBehaviour
     private Rigidbody2D body;
 
     [SerializeField]
+    private CapsuleCollider2D capsuleCollider;
+
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField]
     private float MinX;
     
     [SerializeField]
     private float MaxX;
+
+    [SerializeField]
+    private float DefaultLength = 2;
+
+    [SerializeField]
+    private float TiltRangeMaxSpeed = 8;
+
+    [SerializeField]
+    private float TiltRangeMaxAngleDegrees = 30;
+
+    [SerializeField]
+    private float TiltDamping = 8;
+
+    private float length;
+    private float targetAngle;
+
+    private float Length
+    {
+        get => length;
+        set
+        {
+            length = value;
+
+            spriteRenderer.size = new Vector2(length, spriteRenderer.size.y);
+            capsuleCollider.size = new Vector2(length, capsuleCollider.size.y);
+        }
+    }
     
     void Start()
     {
+        Length = DefaultLength;
+        
         Cursor.visible = false;
     }
     
-    void Update()
+    void FixedUpdate()
     {
         var screenMousePos = Input.mousePosition;
         var mousePos = Camera.main.ScreenToWorldPoint(screenMousePos);
 
-        var x = Mathf.Clamp(mousePos.x, MinX, MaxX);
-        body.MovePosition(new Vector2(x, body.position.y));
+        var min = MinX + Length / 2;
+        var max = MaxX - Length / 2;
+        var x = Mathf.Clamp(mousePos.x, min, max);
+
+        var prevPosition = body.position;
+        var position = new Vector2(x, body.position.y);
+        body.MovePosition(position);
+
+        var velocity = (position - prevPosition) / Time.deltaTime;
+        var speed = Mathf.Clamp(velocity.x, -TiltRangeMaxSpeed, TiltRangeMaxSpeed);
+        var angle = math.remap(-TiltRangeMaxSpeed, TiltRangeMaxSpeed, -TiltRangeMaxAngleDegrees,
+            TiltRangeMaxAngleDegrees, speed);
+
+        targetAngle = -angle;
+        var currentAngle = body.rotation;
+        currentAngle = Mathf.LerpAngle(targetAngle, currentAngle, Mathf.Exp(-TiltDamping * Time.deltaTime));
+        
+        body.MoveRotation(currentAngle);
     }
 }
